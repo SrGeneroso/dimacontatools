b550ad221d304cc5b65a55d008322f2716c6ef2a
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2024-06-13 20:21:50 +0200

    first commit, open props and xlsx

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to dimatools</h1>
+<p>Visit <a href="/contacsvtoxls">csv to xls</a> to convert files</p>
---
+++ src/routes/contacsvtoxls/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<h1>lets see this</h1>
